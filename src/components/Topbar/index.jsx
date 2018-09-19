520ac70dea3bbb3945b143ba215e0803611b222f
aspanio <aspanio@outlook.com>
2018-09-19 21:02:02 +0300

    added href numb

--- src/components/Topbar/index.jsx
+++ src/components/Topbar/index.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './styles.scss';
 
 import logo from './imgs/logo.png';
 
-const Topbar = () => (
-  <nav style={{ backgroundColor: '#fff' }}>
-    <div className={styles.container}>
-      <img src={logo} alt="PROCODE" className={styles.logo} />
-      <h3>8 800 505-72-45</h3>
-    </div>
-  </nav>
-);
+class Topbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { number } = this.props;
+    const noSpacesNumber = number.replace(/ /g, '');
+    const anchorNumber = `tel: ${noSpacesNumber}`;
+    return (
+      <nav style={{ backgroundColor: '#fff' }}>
+        <div className={styles.container}>
+          <img src={logo} alt="PROCODE" className={styles.logo} />
+          <a href={anchorNumber} /><h3>{number}</h3>
+        </div>
+      </nav>
+    );
+  }
+}
+
+Topbar.defaultProps = {
+  number: '+380 97 172 8774',
+};
 
 export default Topbar;
